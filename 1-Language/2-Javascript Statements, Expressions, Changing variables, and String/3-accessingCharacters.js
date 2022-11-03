80688bd825493a132fc57b3bfe1eeb031324ceaf
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2022-11-03 11:25:25 +0100

    Assignment

---
+++ 1-Language/2-Javascript Statements, Expressions, Changing variables, and String/3-accessingCharacters.js
@@ -0,0 +1,13 @@
+let str = 'Helloep';
+
+console.log(str.length)//5
+
+console.log(str[0]) // H
+console.log(str[1]) // e
+console.log(str[3]) // l
+console.log(str[4])  // o
+
+console.log(str.length - 1) // 4
+
+console.log(str[str.length - 1]) // p
+
---
+++ 1-Language/2-Javascript Statements, Expressions, Changing variables, and String/4-assignmentOperators.js
@@ -0,0 +1,39 @@
+let Name = 'Max'
+
+console.log(Name) // Max
+
+ Name = 'Tommy'
+
+console.log(Name) // Tommy
+
+
+let a = 3
+let b = 4
+
+let res = a + b
+
+console.log(res) // 7
+
+
+a += b // a + b
+
+console.log(a) // 7
+
+
+a -= 2 // a - 2
+
+console.log(a) // 5
+
+a++
+
+console.log(a) // 6
+
+a++
+a++
+
+console.log(a) // 8
+
+a++ // a = 8 + 1 => 9
+a-- // a = 9 - 1 => 8
+
+console.log(a) // 8
