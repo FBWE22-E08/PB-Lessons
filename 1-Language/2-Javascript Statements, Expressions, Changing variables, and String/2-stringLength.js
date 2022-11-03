53dc3c539ef15f1e06fe50614c0bd16446a1e477
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2022-11-03 10:58:22 +0100

    string length

---
+++ 1-Language/2-Javascript Statements, Expressions, Changing variables, and String/2-stringLength.js
@@ -0,0 +1,12 @@
+let logoName = 'CRTIL " !'
+
+
+console.log(logoName.length) // 9
+
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+console.log(text.length) // 26
+
+
+console.log('My name is Sima'.length) // 15
