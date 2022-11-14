d1ac408e47f6c43f43aceb9fe8a17ad4c4d9849c
Andrej Tacigin <andrej.tacigin@digitalcareerinstitute.org>
2022-11-14 10:32:28 +0100

    add loops

---
+++ 2-Program/3 - Loops/index.js
@@ -0,0 +1,18 @@
+// First iteration
+// let i = 0
+// 0 < 5
+// Second iteration
+// let i = 1;
+// 1 < 5
+// Third iteration
+// let i = 2
+// Sixth iteration
+// let i = 5
+//  5 < 5
+for (let i = 0; i <= 5; i++) {
+  console.log("hi and i is", i);
+}
+
+for (let i = 5; i > 0; i--) {
+  console.log("hi and i is", i);
+}
